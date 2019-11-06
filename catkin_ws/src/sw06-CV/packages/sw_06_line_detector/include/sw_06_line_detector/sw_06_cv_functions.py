8506538adef9ae4f2b1082f07d38d607185fe174
Ramon Emiliani <ramon.emiliani@umontreal.ca>
2019-11-06 09:51:53 -0500

    Include category 1 functions

--- catkin_ws/src/sw06-CV/packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
+++ catkin_ws/src/sw06-CV/packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
@@ -1,25 +1,47 @@
 import cv2
 import numpy as np
 import rospy
-
+from scipy.signal import convolve2d
 ## Software Exercise 6: Choose your category (1 or 2) and replace the cv2 code by your own!
 
 ## CATEGORY 1
 def inRange(hsv_image, low_range, high_range):
-	return cv2.inRange(hsv_image, low_range, high_range)
+	lower = (hsv_image >= low_range.reshape(1, 1, 3)).all(axis=-1)
+	upper = (hsv_image <= high_range.reshape(1, 1, 3)).all(axis=-1)
+	return np.uint8(255 * lower * upper)
+
 
 def bitwise_or(bitwise1, bitwise2):
-	return cv2.bitwise_or(bitwise1, bitwise2)
+	return np.uint8((bitwise1 + bitwise2) >= 0.5)
+
 
 def bitwise_and(bitwise1, bitwise2):
-	return cv2.bitwise_and(bitwise1, bitwise2)
+	return bitwise1 * bitwise2
+
 
 def getStructuringElement(shape, size):
-	return cv2.getStructuringElement(shape, size)
+	if shape == 0:
+		structuring_element = np.ones(size, dtype='uint8')
+	elif shape == 1:
+		rows, columns = size
+		structuring_element = np.zeros(size, dtype='uint8')
+		structuring_element[rows // 2, :] = 1
+		structuring_element[:, columns // 2] = 1
+	else:
+		rows, columns = size
+		structuring_element = np.ones((rows - 2, columns - 2), dtype='uint8')
+		structuring_element = np.pad(structuring_element, 1, 'constant', constant_values=0)
+		structuring_element[0, columns // 2] = 1
+		structuring_element[-1, columns // 2] = 1
+		structuring_element[rows // 3:-(rows // 3), 0] = 1
+		structuring_element[rows // 3:-(rows // 3), -1] = 1
+
+	return structuring_element
+
 
 def dilate(bitwise, kernel):
-	return cv2.dilate(bitwise, kernel)
-
+	kernel = np.flip(np.flip(kernel, axis=0), axis=1)
+	return np.uint8(convolve2d(bitwise, kernel, 'same') > 0)
 
 
 ## CATEGORY 2
